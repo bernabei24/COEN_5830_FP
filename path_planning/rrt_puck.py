b7f5d9f9510d8b1b0cf88c961bb9f76a6438c722
bernabei24 <bernabei24@gmail.com>
2024-12-06 22:06:46 -0700

    RRT is finding the goal now.  Code ain't pretty but seems to workish.

--- path_planning/rrt_puck.py
+++ path_planning/rrt_puck.py
@@ -1,11 +1,13 @@
 """rrt_puck controller."""
-
-
 # You may need to import some classes of the controller module. Ex:
 from controller import Robot, Motor, DistanceSensor
 import struct
 import random
 import time
+
+ 
+TIME_STEP = 32 # milliseconds
+ 
 
 def monte_carlo_propagate():
     # Initialize success to True
@@ -15,27 +17,19 @@ def monte_carlo_propagate():
     left.setPosition(float('inf'))
     right.setPosition(float('inf'))
     
-   
     #Set the desired duration of random motion (in seconds)
-    duration = random.randint(1, 3)
-    #duration = 30
-     
-    
-     
+    duration = random.randint(1, 10)
+
     #Assign a random velocity to the left motor
     #Assign a random velocity to the right motor
     left_speed = random.uniform(-5.28, 5.28)
     right_speed = random.uniform(left_speed-1, left_speed+1)
-    left.setVelocity(left_speed)
-    right.setVelocity(right_speed)
-    
+        
     #Enable the proximity sensors on the e-puck 
     proximity_sensors = []
     sensor_names = ['ps0', 'ps1', 'ps2', 'ps3', 'ps4', 'ps5', 'ps6', 'ps7']
-    
-    timestep = 32
-    
-    
+    timestep = TIME_STEP
+        
     for name in sensor_names:
         sensor = robot.getDevice(name)
         sensor.enable(timestep)
@@ -43,66 +37,95 @@ def monte_carlo_propagate():
      
     
     start_time = robot.getTime()
+    left.setVelocity(left_speed)
+    right.setVelocity(right_speed)
    
     while robot.step(timestep) != -1:
-        current_time = robot.getTime()
-        elapsed_time = current_time - start_time
-        
-        
         
         
         sensor_values = [sensor.getValue() for sensor in proximity_sensors]
         for i,value in enumerate(sensor_values):
             #Perform collision checking at each iteration. Return False if collision occurs.
-            if value > 300:
+            if value > 100:
                 # print collision time and sensor value
                 # print('Collision time:', elapsed_time)
                 # print('Sensor value:', value)
-                return False, [left_speed, right_speed]
-                 
-        
-        # Generate random velocities for the left and right motors every 1 second
-        if elapsed_time % .5 == 0:
-          left_speed = random.uniform(-5.28, 5.28)
-          right_speed = random.uniform(left_speed-1, left_speed+1)
-          left.setVelocity(left_speed)
-          right.setVelocity(right_speed)
-   
-        
+                return False, -1.0, [left_speed, right_speed]
+            
+        current_time = robot.getTime()
+        elapsed_time = current_time - start_time
         if elapsed_time < duration:
             pass
         else:
             #If no collision occurs, stop the robot, return success and return the velocities.
-            print('No collision during random motion, returning velocities and SUCCESS') 
+            # print('No collision during random motion, returning velocities and SUCCESS') 
             left.setVelocity(0)
             right.setVelocity(0)
-            return True, [left_speed, right_speed]
+            current_time = robot.getTime()
+            elapsed_time = current_time - start_time
+            
+            return True, elapsed_time, [left_speed, right_speed]
+      
+    
+   
+def replay_velocties(velocities):
+
+    #Replay the final path on the robot
+    left = robot.getDevice('left wheel motor')
+    right = robot.getDevice('right wheel motor')
+    left.setPosition(float('inf'))
+    right.setPosition(float('inf'))
+    
+    timestep = TIME_STEP 
+    robot.step(timestep)
+ 
+    while robot.step(timestep) != -1:
+      for velocity in velocities:
         
-        time.sleep(0.015)
-    
-   
-   
-   
-    
-    
-    
-   
-    
-    
+        start_time = robot.getTime()
+       
+        print('Replaying velocities:', velocity)
+        
+        # Talked with Srikrishna about this part of the code
+        # THIS IS NOT LOCALIZATION PROBLEM, AND THEREFORE
+        # WE ARE NOT EXPECTING VELOCITY PLACKBACK TO BE PERFECT
+        # AND WE DONT NEED TO PLAYBACK VELOCITIES, SO DONT!!!!!
+        #
+        #
+        # left.setVelocity(velocity[2] )
+        # right.setVelocity(velocity[3] )
+        
+        while robot.step(timestep) != -1:
+            current_time = robot.getTime()
+            elapsed_time = current_time - start_time
+            # print('Elapsed time:', elapsed_time)
+            if elapsed_time <= (velocity[1]):
+               pass
+            else:
+            #    left.setVelocity(0)
+            #    right.setVelocity(0)
+               break
+           
+        start_time = robot.getTime()  
+        while robot.step(timestep) != -1:
+            current_time = robot.getTime()
+            elapsed_time = current_time - start_time
+            if elapsed_time < 1:
+              right.setVelocity(0)
+              left.setVelocity(0)
+            else:
+              break
+          
+       
+      break 
+  
+    # stop the robot
+    left.setVelocity(0)
+    right.setVelocity(0)
+            
+        
 
 
-#main
-#Initialize the robot, obtain variables for the emitter and receiver on the robot.
-#Set emitter and receiver channels
-#In a while loop:
-	#Check if receiver has received data
-	#If data asks for monte carlo propagation:
-		#perform monte carlo propagation from the robot's current state until 5 successful propagations are obtained.
-		#Use the emmiter to send the following after each propagation attempt: success of propagation (boolean), left motor velocity and right motor velocity.
-        #If data indicates that the goal has been reached, then write code to find the final path and replay the final set of actions on the robot all the way from its initial state to the goal state
-
-
-# move webots e-puck robot
 robot = Robot()
 timestep = int(robot.getBasicTimeStep())
 left_motor = robot.getDevice('left wheel motor')
@@ -124,17 +147,67 @@ receiver = robot.getDevice('receiver')
 emitter.setChannel(10)
 receiver.enable(10)
 
+GOAL_REACHED = False
+REPLAY_COMPLETE = False
+
+final_velocities = []
+
 # Run the simulation step by step in a while loop:
 while robot.step(timestep) != -1:
     if receiver.getQueueLength() > 0:
         message = receiver.getBytes()
         # print('Received message:', message)
         receiver.nextPacket()
-        message = struct.unpack('f', message)
-        # print('Unpacked message:', message)
-        if message == (1.0,):
-            # print('Message is,', message)
-            success, action = monte_carlo_propagate()
+        
+        # Unpack the message first 4 bytes
+        messageStart = struct.unpack('f', message[:4])
+        
+        #If data asks for monte carlo propagation:
+        if messageStart == (1.0,) and not GOAL_REACHED:
+            #perform monte carlo propagation from the robot's current state until 5 successful propagations are obtained.
+            success, duration, action = monte_carlo_propagate()
             print(success, action, type(action))
-            data_bytes = struct.pack('fff', float(success), action[0], action[1])
+            data_bytes = struct.pack('ffff', float(success), float(duration), action[0], action[1])
+            # Use the emmiter to send the following after each propagation attempt: 
+            # success of propagation (boolean), left motor velocity and right motor velocity.
             emitter.send(data_bytes)
+            
+            
+##########################################################################################################
+# If data indicates that the goal has been reached, 
+# then write code to find the final path and replay the final 
+# set of actions on the robot all the way from its initial state to the goal state
+##########################################################################################################            
+        elif messageStart == (2.0,) and not GOAL_REACHED:
+            GOAL_REACHED = True
+            print('Goal reached')
+            
+        elif messageStart == (3.0,):
+            print('Receiving final path')
+            
+            #left_motor.setVelocity(0.0)
+            #right_motor.setVelocity(0.0)
+            
+            # Receive the final path from the supervisor
+            velocities = struct.unpack('ffff', message)
+            print('Final velocities:', velocities)
+            
+            # add last 2 velocities to final velocities
+            final_velocities.append(velocities)
+
+            #left_motor.setVelocity(velocities[0])
+            #right_motor.setVelocity(velocities[1])
+            # Replay the final path on the robot
+            
+        elif messageStart == (4.0,):
+            replay_velocties(final_velocities)
+            print('Replay complete')
+            emitter.send(struct.pack('f', float(5.0)))
+            break
+             
+             
+##########################################################################################################
+# End of code to replay the final set of actions on the robot
+########################################################################################################## 
+       
+            
--- path_planning/rrt_supervisor.py
+++ path_planning/rrt_supervisor.py
@@ -1,30 +1,38 @@
 # You may need to import some classes of the controller module. Ex:
-from controller import Robot, Motor, DistanceSensor
+from controller import Robot, Motor, DistanceSensor, Display
 from controller import Supervisor
 from controller import Node
 import struct
 import time
 import random
 
+# Tunnable parameters for the RRT algorithm
 AREA_SIZE_MAX_Width = 0.9
 AREA_SIZE_MAX_Height = 0.9
 AREA_SIZE_MIN_Width = 0.3
 AREA_SIZE_MIN_Height = 0.3
-
 goal = [0.6, 0.6, 0.0]
+GOAL_COLLISION_THRESHOLD = 0.1
 
-#Create a class to represent a Node, which consists of position, parent Node and action(velocity of the motors)
+
 class Node:
-    def __init__(self, position, parent, action):
+    """
+    A class to represent a node in the tree that holds the position, parent, action and duration of the robot at that node.
+    """
+    def __init__(self, position, parent, action, duration):
         self.position = position
         self.parent = parent
         self.action = action
+        self.duration = duration
         
     def set_parent(self, parent):
         self.parent = parent    
     
     def get_parent(self):
         return self.parent
+    
+    def __str__(self):
+        return str(self.position) + ' ' + str(self.action)
     
 def return_node_closest_to_position(position, tree):
     #Iterate through the tree to find the node closest to the position
@@ -46,7 +54,6 @@ def randomly_sample_state(supervisor):
     print('Randomly sampling state')
     #Using 50% goal biasing, sample a state within the arena and return the state
     new_position = [random.uniform(AREA_SIZE_MIN_Width, AREA_SIZE_MAX_Width), random.uniform(AREA_SIZE_MIN_Height, AREA_SIZE_MAX_Height), 0.0]    
-    
     return new_position
             
 def set_position(supervisor, position, orientation):
@@ -56,24 +63,25 @@ def set_position(supervisor, position, orientation):
     
 def check_if_goal_reached(position):
     #Check if the position is close enough to the goal
-    if calculate_distance(position, goal) < 0.15:
+    if calculate_distance(position, goal) < GOAL_COLLISION_THRESHOLD:
         return True
     return False
 
+ 
+    
 
 def main():
      
-    #Initialize the supervisor node, the graph and create a node class object to represent the initial state of the robot, which is also the only node in the tree
+    #Initialize the supervisor node, the graph and create a node class object 
+    #to represent the initial state of the robot, which is also the only node in the tree
     supervisor = Supervisor()
-    
+ 
     # Get initial position of the robot
     puck = supervisor.getFromDef("e-puck")
     initial_position = puck.getField('translation').getSFVec3f()
      
-    
-    root_node = Node(initial_position, None, [0.0, 0.0])
+    root_node = Node(initial_position, None, [0.0, 0.0], 0.0)
     tree = [root_node]
-    
     
     #Create variables for the e-puck robot, the emmiter and the receiver
     puck = supervisor.getFromDef("e-puck")
@@ -84,10 +92,12 @@ def main():
     emitter.setChannel(10)
     receiver.enable(10)
     
-    timestep = int(supervisor.getBasicTimeStep())
+    timestep = 32 #int(supervisor.getBasicTimeStep())
     
     #Use the emitter to request the puck's controller to perform monte carlo propagations
     receiver.enable(timestep)
+    
+    end_simulation = False
     
     # Run the simulation step by step in a while loop:
     while supervisor.step(timestep) != -1:
@@ -104,9 +114,8 @@ def main():
         nearest_node = return_node_closest_to_position(new_position, tree)
         
         # set the position of the puck to the nearest node
-        set_position(supervisor, nearest_node.position, [0, 1, 0, 0])
+        set_position(supervisor, nearest_node.position, [0, 0, 1, 0])
     
-        
         successful_propagations = []
         five_successful_propagations = False
         
@@ -117,39 +126,28 @@ def main():
                 continue
             
             data = receiver.getBytes()
+            success, duration, left_motor_velocity, right_motor_velocity = struct.unpack('ffff', data)
             
-            
-            success, left_motor_velocity, right_motor_velocity = struct.unpack('fff', data)
             if success:
-              print('Success in propagation')
-              # save robot position
               position = puck.getPosition()
- 
-            #   print('Position:', position)
-            #   print('left motor velocity:', left_motor_velocity)    
-            #   print('right motor velocity:', right_motor_velocity)
-     
-              node = Node(position, None, [left_motor_velocity, right_motor_velocity])
+
+              node = Node(position, None, [left_motor_velocity, right_motor_velocity], duration)
               successful_propagations.append(node)
               
               five_successful_propagations = len(successful_propagations) 
-              if five_successful_propagations == 5:
-                    print('Five successful propagations')
-                    five_successful_propagations = True
-                    
-              
-              print('Propagation length:', len(successful_propagations))
+              if five_successful_propagations == 5: 
+                five_successful_propagations = True
+                break
+
             
             else:
-              print('Collision detected by the supervisor')
               message = struct.pack('f', float(0.0))
-              print('Sending message:', message)
               emitter.send(message)
               break
               
         
              
-        # position in successful_propagations closed to new_position
+        # position in successful_propagations closest to new_position
         if five_successful_propagations:
           closest_node = return_node_closest_to_position(new_position, successful_propagations)
           print('Closest node:', closest_node.position)
@@ -159,30 +157,49 @@ def main():
           closest_node.set_parent(nearest_node)
           tree.append(closest_node)
           print('Tree length:', len(tree))
+        
           
           #Check if the goal has been reached
           if check_if_goal_reached(closest_node.position):
-            print('Goal reached!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-            #sleep for 1 hour
-            #time.sleep(3600)
-            break    
+            
+            # print goal reached in ascii art to the console
+            print('Goal reached')
+            
+            # sleep for 10 seconds
+            time.sleep(5)
+            
+            #print the final path
+            final_path = []
+            node = closest_node
+            while node:
+                final_path.append(node)
+                node = node.get_parent()
+            final_path.reverse()
+            
+            message = struct.pack('f', float(2.0))
+            emitter.send(message)
+            
+            #Send the final path to the puck's controller
+            # for node in final_path:
+            for i  in range(len(final_path)):
+                # create a message to send the final path to the puck's controller
+                message = struct.pack('ffff', float(3.0), float(final_path[i].duration), final_path[i].action[0], final_path[i].action[1])
+                print('Sending final velocities:', message)
+                # byte length of the message
+                print('Message length:', len(message))
+                emitter.send(message)
+                
+            message = struct.pack('f', float(4.0))
+            emitter.send(message)
+            end_simulation = True
+     
+                            
+        if end_simulation:
+            break
             
         receiver.nextPacket()
-        time.sleep(.015)    
+             
     
-    
-    
-    	
-        #While 5 succcesfull propagations are yet to be executed:
-            #Use the reciever to obtain the success and the velocity values
-            #If success if false, collision has occured. Move on.
-            #Else, obtain the final position of the robot and store it.
-            #Teleport the puck back to the nearest node
-        #Compare the final positions with the randomly sampled state and pick the nearest one.
-        #Add it to the tree
-        #Is the new state close enough to the goal:
-        	#GOAL REACHED! 
-        	#Write code to gather the final set of actions and use emitter to transmit data for replaying
             
 if __name__ == "__main__":
     main()
