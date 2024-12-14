662c4986ccd5683aae199778bd7ff204727467ba
Ian Mcconachie <47074754+nomad82732@users.noreply.github.com>
2024-12-13 21:10:04 -0700

    Extended the code for nxn grid
    I noticed the code may not be working 100%. Ground robots sometimes try to move to the same space, and then they set one robot to its goal location. I noticed this while creating the gifs for the submission and haven't had a chance to try and fix it.

--- object_oriented_programming/robot_warehouse.py
+++ object_oriented_programming/robot_warehouse.py
@@ -4,6 +4,22 @@ import matplotlib.animation as animation
 from matplotlib.animation import FuncAnimation
 import time
  
+"""
+  COEN 5830 - Intro to Robotics
+  Final Project: Object Oriented Programming 
+  Student(s): Michael Bernabei, Ian Mcconachie                                                                       
+  Fall 2024
+  Instructor: Dr. Leopold Beuken
+                                                                                                                                                                
+  Program description: Builds warehouse management simulation.
+
+  Author(s): Michael Bernabei, Ian Mcconachie
+  Date: 11/13/2024
+  
+  precondition: The data.csv file must be in the same directory as this script.  
+  
+"""
+ 
 robot_types = ['QUADCOPTER', 'DIFFERENTIAL_DRIVE', 'HUMANOID']
 legal_moves = [(0, 1), (0, -1), (1, 0), (-1, 0)]
 
@@ -99,14 +115,15 @@ class Djikstra:
                     break
                 
         return shortest_path
-    
-        
+      
 class Robot:
     def __init__(self, x, y, warehouse):
         self.x = x
         self.y = y
         self.robot_type = 'GENERAL'
         self.warehouse = warehouse
+        self.bound_x = warehouse.width
+        self.bound_y = warehouse.height
         self.move_request_x = 0
         self.move_request_y = 0
         self.goal_x = -1
@@ -124,7 +141,7 @@ class Robot:
         self.y = y  
     
     def get_shortest_path(self):
-        djikstra = Djikstra()
+        djikstra = Djikstra(self.bound_x,self.bound_y)
         djikstra.set_goal(self.goal_x, self.goal_y)
         djikstra.set_start(self.x, self.y)
         shortest_path = djikstra.get_shortest_path(self.warehouse)
@@ -147,6 +164,7 @@ class Robot:
         
         if len(shortest_path) == 0:
             return
+        
         elif len(shortest_path) > 1:
             move_x_request = shortest_path[-2][0]
             move_y_request = shortest_path[-2][1]
@@ -154,13 +172,11 @@ class Robot:
             move_x_request = shortest_path[-1][0]
             move_y_request = shortest_path[-1][1]
             
-        
         self.move_request_x = move_x_request
         self.move_request_y = move_y_request
         
     def get_move_request(self):
         return self.move_request_x, self.move_request_y
-    
     
 class Quadcopter(Robot):
     def __init__(self, x, y, warehouse):
@@ -179,11 +195,8 @@ class Humanoid(Robot):
         super().__init__(x, y, warehouse )
         self.color = 'blue'
         self.robot_type = 'HUMANOID'
-        
-        
     
-        
-
+# this is the grid class 
 class Warehouse:
     def __init__(self, width, height):
         self.width = width
@@ -220,30 +233,8 @@ class Warehouse:
             robot.move_to_closest_unoccupied_position()
             return
         
-        
         robot.set_position(x, y)
-        
-        
-    
-    def plot_warehouse(self):
-        plt.imshow(self.warehouse, cmap='gray', vmin=0, vmax=255)
-        for robot in self.robots:
-            plt.plot(robot.x, robot.y, 'ro')
-        
-        self.show()
-        
-    def plot_warehouse_with_goal(self):
-        plt.imshow(self.warehouse, cmap='gray', vmin=0, vmax=255)
-        for robot in self.robots:
-            plt.plot(robot.x, robot.y, 'ro')
-            plt.plot(robot.goal_x, robot.goal_y, 'bo')
             
-        # show all grid lines
-        plt.xticks(np.arange(0, self.width, 1))
-        plt.yticks(np.arange(0, self.height, 1))
-        plt.grid(True)    
-        self.show()
-        
     def add_obstacle(self, x, y):
         self.warehouse[x, y] = 0
         
@@ -265,69 +256,7 @@ class Warehouse:
             robot_positions.append((robot.x, robot.y))
         
         return robot_positions
-        
-        
-    def plot_warehouse_with_goal_path(self):
-        plt.imshow(self.warehouse, cmap='gray', vmin=0, vmax=255)
-        
-        for robot in self.robots:
-    
-            if robot.color == 'red':
-                robo_pos = 'ro'
-                robo_goal = 'rs'
-                robo_path = 'r--'
-            elif robot.color == 'green':
-                robo_pos = 'go'
-                robo_goal = 'gs'
-                robo_path = 'g--'
-            elif robot.color == 'blue':
-                robo_pos = 'bo'
-                robo_goal = 'bs'
-                robo_path = 'b--'
-            
-            plt.plot(robot.x, robot.y, robo_pos)
-            plt.plot(robot.goal_x, robot.goal_y, robo_goal)
-            plt.plot([robot.x, robot.goal_x], [robot.y, robot.goal_y], robo_path)
-            
-            
-        self.show()
-        
-    def plot_warehouse_with_goal_path_type(self):
-        plt.imshow(self.warehouse, cmap='gray', vmin=0, vmax=255)
-        for robot in self.robots:
-            plt.plot(robot.x, robot.y, 'ro')
-            plt.plot(robot.goal_x, robot.goal_y, 'bs')
-            # plot path from robot to goal with dashed line
-            plt.plot([robot.x, robot.goal_x], [robot.y, robot.goal_y], 'g--')
-            plt.text(robot.x, robot.y, robot.type)
-        self.show()
-               
-    def show(self):
-        # show all grid lines
-        plt.gca().invert_yaxis()
-        plt.xticks(np.arange(0, self.width, 1))
-        plt.yticks(np.arange(0, self.height, 1))
-        plt.grid(True)    
-        
-        # create legend for robots
-        handles = []
-        for robot in self.robots:
-            if robot.color == 'red':
-                patch = plt.Line2D([0], [0], marker='o', color='w', label='Quadcopter', markerfacecolor='r', markersize=10)
-            elif robot.color == 'green':
-                patch = plt.Line2D([0], [0], marker='o', color='w', label='Differential Drive', markerfacecolor='g', markersize=10)
-            elif robot.color == 'blue':
-                patch = plt.Line2D([0], [0], marker='o', color='w', label='Humanoid', markerfacecolor='b', markersize=10)
-            
-            handles.append(patch)
-                
-        plt.legend(handles=handles, bbox_to_anchor=(1.3, .6), loc='right')
-        plt.title('Robot Grid Navigation')   
-     
-            
-        
-        plt.show()
-    
+
     def is_position_occupied(self, x, y):
         for robot in self.robots:
             if robot.x == x and robot.y == y  and robot.robot_type != 'QUADCOPTER' :
@@ -367,9 +296,85 @@ class Warehouse:
         # move robots
         for robot, move_x, move_y in request_moves:
             self.move_robot(robot, move_x, move_y)
+
+    def plot_warehouse(self):
+        plt.imshow(self.warehouse, cmap='gray', vmin=0, vmax=255)
+        for robot in self.robots:
+            plt.plot(robot.x, robot.y, 'ro')
+        
+        # self.show()
+        
+    def plot_warehouse_with_goal(self):
+        plt.imshow(self.warehouse, cmap='gray', vmin=0, vmax=255)
+        for robot in self.robots:
+            plt.plot(robot.x, robot.y, 'ro')
+            plt.plot(robot.goal_x, robot.goal_y, 'bo')
+            
+        # show all grid lines
+        plt.xticks(np.arange(0, self.width, 1))
+        plt.yticks(np.arange(0, self.height, 1))
+        plt.grid(True)    
+        # self.show()
+            
+    def plot_warehouse_with_goal_path(self):
+        plt.imshow(self.warehouse, cmap='gray', vmin=0, vmax=255)
+        
+        for robot in self.robots:
+    
+            if robot.color == 'red':
+                robo_pos = 'ro'
+                robo_goal = 'rs'
+                robo_path = 'r--'
+            elif robot.color == 'green':
+                robo_pos = 'go'
+                robo_goal = 'gs'
+                robo_path = 'g--'
+            elif robot.color == 'blue':
+                robo_pos = 'bo'
+                robo_goal = 'bs'
+                robo_path = 'b--'
+            
+            plt.plot(robot.x, robot.y, robo_pos)
+            plt.plot(robot.goal_x, robot.goal_y, robo_goal)
+            plt.plot([robot.x, robot.goal_x], [robot.y, robot.goal_y], robo_path)
             
             
-             
+        self.show()
+        
+    def plot_warehouse_with_goal_path_type(self):
+        plt.imshow(self.warehouse, cmap='gray', vmin=0, vmax=255)
+        for robot in self.robots:
+            plt.plot(robot.x, robot.y, 'ro')
+            plt.plot(robot.goal_x, robot.goal_y, 'bs')
+            # plot path from robot to goal with dashed line
+            plt.plot([robot.x, robot.goal_x], [robot.y, robot.goal_y], 'g--')
+            plt.text(robot.x, robot.y, robot.type)
+        # self.show()
+               
+    def show(self):
+        # show all grid lines
+        plt.gca().invert_yaxis()
+        plt.xticks(np.arange(0, self.width, 1))
+        plt.yticks(np.arange(0, self.height, 1))
+        plt.grid(True)    
+        
+        # create legend for robots
+        handles = []
+        for robot in self.robots:
+            if robot.color == 'red':
+                patch = plt.Line2D([0], [0], marker='o', color='w', label='Quad', markerfacecolor='r', markersize=10)
+            elif robot.color == 'green':
+                patch = plt.Line2D([0], [0], marker='o', color='w', label='Diff Drive', markerfacecolor='g', markersize=10)
+            elif robot.color == 'blue':
+                patch = plt.Line2D([0], [0], marker='o', color='w', label='Humanoid', markerfacecolor='b', markersize=10)
+            
+            handles.append(patch)
+                
+        plt.legend(handles=handles, bbox_to_anchor=(1.35, .6), loc='right')
+        plt.title('Robot Grid Navigation')   
+
+        # plt.show()
+                           
 def generate_random_robot(warehouse):
     x = np.random.randint(0, warehouse.width)
     y = np.random.randint(0, warehouse.height)
@@ -387,8 +392,6 @@ def generate_random_robot(warehouse):
     else:
         return Humanoid(x, y, warehouse)
 
-
- 
 def generate_random_goal(warehouse):
     x = np.random.randint(0, warehouse.width)
     y = np.random.randint(0, warehouse.height)
@@ -399,55 +402,51 @@ def generate_random_goal(warehouse):
     
     return x, y
 
-# create warehouse
-warehouse = Warehouse(5, 5)
+############################################ create warehouse
+n = 3
+warehouse = Warehouse(n, n)
 
 # randomly create 10 robots across the warehouse
 robots = []
-for i in range(10):
+for i in range(2*n):
     robot = generate_random_robot(warehouse)
     warehouse.add_robot(robot)
     robots.append(robot)
 
-
 # randomly set goals for each robot
 for robot in robots:
     x, y = generate_random_goal(warehouse)
+    p1 = np.array([x, y])
+    p2 = np.array([robot.x, robot.y])
+    dist = np.linalg.norm(p1 - p2)
+    
+    if n > 4:
+        while dist < 2 :
+            x, y = generate_random_goal(warehouse)
+            p1 = np.array([x, y])
+            dist = np.linalg.norm(p1 - p2)
+        
+    # print(dist)
     robot.set_goal(x, y)
  
-
-
-# animate robots moving to their goals
+# # animate robots moving to their goals
 def animate(i):
     
-    if i == 0:
-        # pause the screen so we can see the initial state
-        time.sleep(.5)
-        
-    if i == 1000:
-        print('Animation stopped')
-    
     plt.cla()
-
-    for robot in warehouse.robots:
-        robot.move_towards_goal()
-    
-    warehouse.process_move_request()
     warehouse.plot_warehouse_with_goal_path()
     
-    if len(warehouse.robots) == 0:
-        ani.event_source.stop()
+    for robot in warehouse.robots:
+        robot.move_towards_goal()
         
-
-    return ani
-
+    warehouse.process_move_request()
+    
+    time.sleep(0.75)
+    
+    return warehouse
+time.sleep(10)
+warehouse.plot_warehouse_with_goal_path()
 ani = FuncAnimation(plt.gcf(), animate, frames=30, interval=1000)
-
 plt.show()
 
 
  
-      
-    
-
-
